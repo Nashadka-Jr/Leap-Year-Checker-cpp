4725df1de405609cad5aaaf10241b512d5200627
Nashadka-Jr <82074366+Nashadka-Jr@users.noreply.github.com>
2024-07-25 00:45:24 -0600

    Update leap_year.cpp
    Removed four-digit requirement.

--- leap_year.cpp
+++ leap_year.cpp
@@ -3,7 +3,6 @@
 int main() {
     //Define variables
     double year;
-    bool four_digit;
     double year2; //Temporary variable used for math
     int year3; //Likewise
     double year4; //Ditto
@@ -16,33 +15,25 @@ int main() {
 
     std::cout << "What is the year you want to check?\n";
     std::cin >> year;
-    //Check for four digits
-    if (year < 1000 or year > 9999) {
-        std::cout << "Sorry, I can only check four-digit years (Nashadka Junior is an idiot.)\n";
+    //Check for divisibility by 4
+    year2 = (year / 4);
+    year3 = year2;
+    year8 = year3;
+    if (year2 != year8) {
+        std::cout << "It is not a leap year.\n";
     }
     else {
-        bool four_digit = true;
-        //Check for divisibility by 4
-        year2 = (year / 4);
-        year3 = year2;
-        year8 = year3;
-        if (year2 != year8) {
+        year4 = (year / 100);
+        year5 = year4;
+        year9 = year5;
+        year6 = (year / 400);
+        year7 = year6;
+        year10 =year7;
+        if (year4 == year9 && year6 != year10) {
             std::cout << "It is not a leap year.\n";
         }
         else {
-            year4 = (year / 100);
-            year5 = year4;
-            year9 = year5;
-            year6 = (year / 400);
-            year7 = year6;
-            year10 =year7;
-            if (year4 == year9 && year6 != year10) {
-                std::cout << "It is not a leap year.\n";
-            }
-            else {
-                std::cout << "It is a leap year!\n";
-            }
-
+            std::cout << "It is a leap year!\n";
         }
     }
 }
